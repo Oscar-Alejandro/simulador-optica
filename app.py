4c9c174261963eb2f537a4445c96765373a6d865
Oscar-Alejandro <oscar.alejandro@ciencias.unam.mx>
2026-06-16 20:18:17 -0600

    Agrega estructura multipágina y portada app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Simulador de Óptica UNAM", layout="centered")
+st.title("Laboratorio Virtual de Óptica")
+st.markdown("Bienvenido al simulador. Por favor, selecciona un módulo en el menú lateral izquierdo para comenzar.")
